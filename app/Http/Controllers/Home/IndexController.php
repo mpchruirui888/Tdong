582837d782d46c454e0c69435cf4eb318f4a9d00
mpchrui <1549431732@qq.com>
2020-07-03 12:06:33 +0800

    no message

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -12,8 +12,21 @@ class IndexController extends Controller
 
     public function index()
     {
-        $head =  $header = getallheaders();
-        dd($head);
+        if (!function_exists('getallheaders')) {
+
+            function getallheaders() {
+                $headers = array();
+                foreach ($_SERVER as $name => $value) {
+                    if (substr($name, 0, 5) == 'HTTP_') {
+                        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                    }
+                }
+                return $headers;
+            }
+
+        }
+        var_dump(getallheaders());
+        die;
         return view('home.index');
     }
 
